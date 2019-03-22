89c220ef23ac693bb9ba98f6b9a1857f0cf27f5e
Hang Kim Duyen <duyenhang@poetadigital.com>
2019-03-22 10:41:05 +0700

    changr sound file

--- ios/RNTwilioVoice/RNTwilioVoice.m
+++ ios/RNTwilioVoice/RNTwilioVoice.m
@@ -237,7 +237,7 @@ RCT_REMAP_METHOD(getActiveCall,
                         content.title = [NSString localizedUserNotificationStringForKey:@"Agvisor" arguments:nil];
                         content.body = [NSString localizedUserNotificationStringForKey:@"A farmer is requesting to connect"
                                                                              arguments:nil];
-                        content.sound = [UNNotificationSound soundNamed:@"answer_now_request.mp3"];
+                        content.sound = [UNNotificationSound soundNamed:@"answer_now_request.caf"];
                         content.userInfo = bodyJson;
                         
                         // Deliver the notification
